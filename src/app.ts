cf57202a78e165a6c6bc28dbcc58c3df893f356f
Ivan Labozhevich <labozheviich@gmail.com>
2025-03-26 22:37:51 +0300

    Initial changes and connection to MongoDB

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import config from "./config";
+
+const { port, mongoConnectionUrl } = config;
+const app = express();
+
+app.use(express.json());
+
+mongoose
+  .connect(mongoConnectionUrl)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB", err);
+  });
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
---
+++ src/config.ts
@@ -0,0 +1,8 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export default {
+  port: process.env.PORT || 3001,
+  mongoConnectionUrl: process.env.MONGODB,
+};
